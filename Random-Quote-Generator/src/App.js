aa76ab4b0bed0fe6d542a0817afbc55e61943b12
goumi1009 <goumi1009@gmail.com>
2022-03-10 22:56:03 +0900

    refactor: App 컴포넌트에서 state 관리하도록 수정

--- Random-Quote-Generator/src/App.js
+++ Random-Quote-Generator/src/App.js
@@ -1,7 +1,30 @@
-import Container from './components/Container.js';
+import { requestQuotes } from './api.js';
+import QuoteCard from './components/QuoteCard.js';
+import Button from './components/Button.js';
 
 export default function ({ targetEl }) {
-  new Container({
+  this.state = {};
+  this.setState = (nextState) => {
+    this.state = nextState;
+    quoteCard.setState(this.state);
+  };
+
+  const quoteCard = new QuoteCard({
     targetEl,
+    initialsate: this.state,
   });
+
+  new Button({
+    targetEl,
+    text: 'Generate Quote',
+    onclick: async () => {
+      fetchQuote();
+    },
+  });
+
+  const fetchQuote = async () => {
+    const res = await requestQuotes();
+    this.setState(res);
+  };
+  fetchQuote();
 }
